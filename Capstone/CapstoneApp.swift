f84edecb3368d4328392dee9bec550465b8229b6
Edwin Weru <eduguru.weru@gmail.com>
2025-11-03 09:31:39 +0300

    first commit

---
+++ Capstone/CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Edwin Weru on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
